4f08e35863fba8794b6963cb33c5929d7d7212bf
romain-25 <romain.rouabah@gmail.com>
2019-10-30 10:31:37 +0100

    .

--- Site/assets/js/script.js
+++ Site/assets/js/script.js
@@ -25,4 +25,13 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
+$(document).ready(function() {
+  $('.js-scrollTo').on('click', function() { // Au clic sur un élément
+  var page = $(this).attr('href'); // Page cible
+  var speed = 750; // Durée de l'animation (en ms)
+  $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+  return false;
+  });
+  });
+
 /* fin script de sphotos ***************/
